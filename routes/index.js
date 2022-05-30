a24f80df9331bd16b908174de014cf8fae2a8c85
HoaiBao-kai <phamhoaibao01052001@gmail.com>
2022-05-30 18:14:19 +0700

    a

--- routes/index.js
+++ routes/index.js
@@ -81,8 +81,9 @@ router.post('/dangnhap', loginValidator, (req, res, next) => {
             let current = new Date().getTime();
             let dDate = new Date(results[0].lock_account_day).getTime();
             let time = current - dDate;
-            
-            if(results[0].unusual_signin === 1 && time <6000){
+            console.log('time')
+            console.log(time)
+            if(results[0].unusual_signin === 1 && time <60000){
               req.flash('error', 'Tài khoản hiện đang bị tạm khóa, vui lòng thử lại sau 1 phút')
               return res.redirect('/dangnhap')
             }
